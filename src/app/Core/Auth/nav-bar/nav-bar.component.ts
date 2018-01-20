075be6b94d3c88e97d13092c6816fac42f6d7e42
Odumosu <az6bcn@gmail.com>
2018-01-20 23:57:59 +0100

    Error Fixed, application now starting. Menu Tabs loading correctly

--- src/app/Core/Auth/nav-bar/nav-bar.component.ts
+++ src/app/Core/Auth/nav-bar/nav-bar.component.ts
@@ -9,68 +9,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-private menuStrategyArray: Array<string> = new Array<string>();
-public menuToLoad: Array<string>
-public isLoggedIn$ = new BehaviorSubject<boolean>(false);
+  private menuStrategyArray: Array<string> = new Array<string>();
+  public menuToLoad: Array<string>
+  public isLoggedIn$ = new BehaviorSubject<boolean>(false);
 
   constructor(private authService: AuthService,
-              private router: Router,
-              private activatedRoute: ActivatedRoute) { }
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-
-    if(this.authService.checkLocalStorageForToken()){
-      this.getLoggedInUserRole();
-    }
     this.hideLogin();
   }
 
-private getLoggedInUserRole(){
-  console.log("check for token: ", this.authService.checkLocalStorageForToken());
-  const loggedInUserClaims = this.authService.getLoggedInUserClaims();
+  private getLoggedInUserRole() {
+    const loggedInUserClaims = this.authService.getLoggedInUserClaims();
 
-  if (loggedInUserClaims && loggedInUserClaims.Role){
-  this.menuToLoad= this.loadMenu(loggedInUserClaims.Role)
+    if (loggedInUserClaims && loggedInUserClaims.Role) {
+      this.menuToLoad = this.loadMenu(loggedInUserClaims.Role)
 
-  this.isLoggedIn$.next(true);
-  console.log("this.menuToLoad", this.menuToLoad);
-}
-}
- 
-  private loadMenu(role: string){
+      this.isLoggedIn$.next(true);
+    }
+  }
+
+  private loadMenu(role: string) {
     return this.menuStrategy(role);
   }
 
-  private menuStrategy(role: string){
-    this.menuStrategyArray['GeneralManager'] = [{"Title": "Customers", "route": "/customer-manager/general-manager"},
-                    {"Title": "Products", "route": "/customer-manager/products"}, {"Title": "Orders", "route": "customer-manager/orders"}];
-    
-    this.menuStrategyArray['SectionManager'] = [{"Title": "Customers", "route": "/customer-manager/general-manager"},
-                    {"Title": "Products", "route": "customer-manager/products"}, {"Title": "Orders", "route": "customer-manager/orders"}];
-    
-    this.menuStrategyArray['ProductManager'] = [{"Title": "Customers", "route": "/customer-manager/general-manager"},
-                    {"Title": "Products", "route": "customer-manager/products"}, {"Title": "Orders", "route": "customer-manager/orders"}];
-    
-    this.menuStrategyArray['SupplyManager'] = [{"Title": "Customers", "route": "/customer-manager/general-manager"},
-                    {"Title": "Products", "route": "customer-manager/products"}, {"Title": "Orders", "route": "customer-manager/orders"}];
-    
-                    return this.menuStrategyArray[role];
+  private menuStrategy(role: string) {
+    this.menuStrategyArray['GeneralManager'] = [{ "Title": "Customers", "route": "/customer-manager/general-manager" },
+    { "Title": "Products", "route": "/customer-manager/products" }, { "Title": "Orders", "route": "customer-manager/orders" }];
+
+    this.menuStrategyArray['SectionManager'] = [{ "Title": "Customers", "route": "/customer-manager/general-manager" },
+    { "Title": "Products", "route": "customer-manager/products" }, { "Title": "Orders", "route": "customer-manager/orders" }];
+
+    this.menuStrategyArray['ProductManager'] = [{ "Title": "Customers", "route": "/customer-manager/general-manager" },
+    { "Title": "Products", "route": "customer-manager/products" }, { "Title": "Orders", "route": "customer-manager/orders" }];
+
+    this.menuStrategyArray['SupplyManager'] = [{ "Title": "Customers", "route": "/customer-manager/general-manager" },
+    { "Title": "Products", "route": "customer-manager/products" }, { "Title": "Orders", "route": "customer-manager/orders" }];
+
+    return this.menuStrategyArray[role];
   }
 
 
-private logOut(){
-  this.authService.logOut();
-  this.isLoggedIn$.next(false);
+  private logOut() {
+    this.authService.logOut();
+    this.isLoggedIn$.next(false);
 
-  this.hideLogin();
+    this.hideLogin();
 
-  this.router.navigate(['/account/login'], {relativeTo: this.activatedRoute});
-}
+    this.router.navigate(['/account/login'], { relativeTo: this.activatedRoute });
+  }
 
-/**
- * Hides the menus when no token in local storage
- */
-private hideLogin(){
- return this.authService.checkLocalStorageForToken();
-}
+  /**
+   * Hides the menus when no token in local storage
+   */
+  private hideLogin() {
+    this.LoadMenuTabs();
+    
+    return this.authService.checkLocalStorageForToken();
+  }
+
+  private LoadMenuTabs() {
+    if(this.authService.checkLocalStorageForToken()){
+      this.getLoggedInUserRole();
+    }
+  }
 }
